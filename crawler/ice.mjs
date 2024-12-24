8bb4bead8400df7869ba4d0cee99d6e440be1898
gnehs <gnehs@gnehs.net>
2024-12-24 23:13:56 +0800

    Add ice data processing script and update package.json with new test command

---
+++ crawler/ice.mjs
@@ -0,0 +1,32 @@
+import fs from "fs";
+function readJSON(path) {
+  return JSON.parse(fs.readFileSync(path, "utf8"));
+}
+const sevenEleven = readJSON("dist/7-11.json");
+const familyMart = readJSON("dist/family-mart.json");
+
+const result = [
+  ...sevenEleven
+    .filter((store) => store.StoreImageTitle.includes("24雪淋霜霜淇淋"))
+    .map((store) => ({
+      name: store.POIName,
+      address: store.Address,
+      lat: store.Y / 1000000,
+      lng: store.X / 1000000,
+      brand: "7-11",
+      tags: ["霜淇淋"],
+    })),
+  ...familyMart
+    .filter((store) => store.all?.includes("ice"))
+    .map((store) => ({
+      name: store.NAME,
+      address: store.addr,
+      lat: store.py,
+      lng: store.px,
+      brand: "全家",
+      tags: ["霜淇淋", store.twoice === "Y" ? "雙口味" : "單口味"],
+    })),
+];
+
+if (!fs.existsSync("dist")) fs.mkdirSync("dist", { recursive: true });
+fs.writeFileSync("dist/ice.json", JSON.stringify(result, null, 2));
